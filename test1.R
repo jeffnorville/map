4310116f75d1c7be4623980b15506588f2e03305
Jeff Norville <jeff@jnorville.com>
2019-01-28 07:15:10 +0100

    enum dataframe; verify struct names, completeness

--- test1.R
+++ test1.R
@@ -3,51 +3,29 @@
 
 library(R.matlab)
 #setwd("C:/Users/Norville/Documents/spatial_data/sadapt")
-sars <- readMat('SARs4EA.mat')
+sars <- readMat('SARs4EA.mat', fixNames = TRUE)
+#sars2 <- readMat('SARs4EA.mat')
+#all.equal(sars, sars2)
 
 d <- sars$SARs4EA
 df <- as.data.frame(d)
+df$`1.1`
+
+dft <- t(df)
+#wups https://stackoverflow.com/questions/6778908/transpose-a-data-frame
+
+head(dft)
+head(df)
+
+dft$`1.1`
 
 
-dgeometry <- d[[1]]
-dx        <- d[[2]]
-dy        <- d[[3]]
-dcodepra  <- d[[4]]
-
-
-
-class(sars) #it's a list
-head(sars)
-attr(sars, "Geometry")
-attr(sars, "X")
-attr(sars, "Q")
+#http://lukaspuettmann.com/2017/03/13/matlab-struct-to-r-dataframe/
+varNames        <- names(sars$SARs4EA[,,1])
+datList         <- sars$SARs4EA
+datList         <- lapply(datList, unlist, use.names=FALSE) 
+sarsdata        <- as.data.frame(datList) #where wheels come off - datList unlisting doesn't appreciate (what part?) complexity of file
+names(sarsdata) <- varNames
 
 
 #table(unlist(lapply(dta, class)))
-
-library(rgdal)
-library(ggmap)
-library(ggplot2)
-library(maps)
-library(mapdata)
-library(broom)
-
-france <- map_data("france")
-ggplot() + geom_polygon(data = france, aes(x=long, y = lat, group = group)) + 
-  coord_fixed(1.2)
-
-ex1 <- unlist(sars, use.names = TRUE)
-
-head(ex1)
-
-
-ex1[1]
-class(ex1)
-
-head(tidy(sars))
-head(structure(sars)[1])
-dput(sars)
-
-geometrystuff <= structure(sars)[1]
-
-
---
+++ testMap.R
@@ -0,0 +1,28 @@
+
+library(rgdal)
+library(ggmap)
+library(ggplot2)
+library(maps)
+library(mapdata)
+library(broom)
+
+france <- map_data("france")
+ggplot() + geom_polygon(data = france, aes(x=long, y = lat, group = group)) + 
+  coord_fixed(1.2)
+
+
+ex1 <- unlist(sars, use.names = TRUE)
+head(ex1)
+
+ex1[1]
+class(ex1)
+
+tiday()
+head(tidy(df))
+
+head(structure(df)[1])
+dput(df)
+
+geometrystuff <= structure(sars)[1]
+
+
