bdacd327d7d94ca4513fc0544062f34d2f63f7ed
jeffnorville <jeff@jnorville.com>
2019-02-07 16:31:32 +0100

    digging

---
+++ corentin.R
@@ -0,0 +1,4 @@
+# corentin's work:
+
+load("~/spatial_data/agronomie/corentin_data/aOM.rda")
+load("~/spatial_data/agronomie/dataModeleCorentinRendement/Super_Table_classique.rda")
--- test1.R
+++ test1.R
@@ -33,5 +33,57 @@ datList         <- lapply(datList, unlist, use.names=FALSE) #tried keeping names
 sarsdata        <- as.data.frame(datList) #where wheels come off - datList unlisting doesn't appreciate (what part?) complexity of file "les arguments impliquent des nombres de lignes différents"
 names(sarsdata) <- varNames
 
+row.names(df)
 
 #table(unlist(lapply(dta, class)))
+
+## things I need to access...
+MyDepartments: df[37,]
+mydepts <- df[37,]
+MyRegions: df[36,]
+
+# LandUses
+# df[22,]
+landuses <- df[22,]
+
+
+# $CODEPRA
+# like this: df[4,] (and they are 1.1 through 709.1)
+codepra <- df[4,]
+
+# 
+# $RemainingCLC2012
+# like this: df[15,]
+# 
+# CLC111 300.6659
+# CLC112 14127.81
+# CLC121 3167.867
+# CLC122 581.435 
+# CLC123 143.278 
+# CLC124 558.8838
+# CLC131 30.27001
+# CLC132 0       
+# CLC133 0       
+# CLC141 912.4413
+# CLC142 725.2852
+# CLC221 0       
+# CLC222 31.27901
+# CLC223 0       
+# CLC242 454.8634
+# CLC243 122.0344
+# CLC322 0       
+# CLC331 0       
+# CLC332 0       
+# CLC333 0       
+# CLC334 0       
+# CLC335 0       
+# CLC411 0       
+# CLC412 0       
+# CLC421 0       
+# CLC422 0       
+# CLC423 0       
+# CLC511 241.7534
+# CLC512 96.86403
+# CLC521 0       
+# CLC522 0       
+# CLC523 0 
