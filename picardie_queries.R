f15000279ad0cf4728b289f15ce55b5094d4844c
jeffnorville <jeff@jnorville.com>
2019-03-26 15:42:29 +0100

    writeogr consolidation

--- picardie_queries.R
+++ picardie_queries.R
@@ -214,22 +214,6 @@ setwd(dir = 'C:/opt/out_rpgs')
 library(rgdal)
 ogrDrivers()
 
-writeOGR(ilots_2008_002, dsn = "ilots_2008_002.shp", driver="ESRI Shapefile", verbose = TRUE)
-#FAILS writeOGR(ilots_2008_002, "ilots_2008_002", layer = "data", driver="PostgreSQL", verbose = TRUE)
-#WORKS, doesn't contain much:
-writeOGR(ilots_2008_002, "ilots_2008_002", layer = "data", driver="SQLite", verbose = TRUE)
-
-#Works, looked the same during processing, has all polygons
-writeOGR(ilots_2008_002, dsn="ilots_2008_002", layer="ilots_2008_002", driver="ESRI Shapefile", verbose = TRUE)
-
-
-head(ilots_2008_002)
-
-
-try(writeOGR(cities, td, "cities", driver="ESRI Shapefile"))
-writeOGR(cities, td, "cities", driver="ESRI Shapefile", overwrite_layer=TRUE)
-
-
 
 
 ggplot(ilots_2008_002)
--- rdaToShp.R
+++ rdaToShp.R
@@ -1,2 +1,18 @@
 #import .rda file, export .shp file
 
+writeOGR(ilots_2008_002, dsn = "ilots_2008_002.shp", driver="ESRI Shapefile", verbose = TRUE)
+#FAILS writeOGR(ilots_2008_002, "ilots_2008_002", layer = "data", driver="PostgreSQL", verbose = TRUE)
+#WORKS, doesn't contain much:
+writeOGR(ilots_2008_002, "ilots_2008_002", layer = "data", driver="SQLite", verbose = TRUE)
+
+#Works, looked the same during processing, has all polygons
+writeOGR(ilots_2008_002, dsn="ilots_2008_002", layer="ilots_2008_002", driver="ESRI Shapefile", verbose = TRUE)
+
+
+head(ilots_2008_002)
+
+
+try(writeOGR(cities, td, "cities", driver="ESRI Shapefile"))
+writeOGR(cities, td, "cities", driver="ESRI Shapefile", overwrite_layer=TRUE)
+
+
