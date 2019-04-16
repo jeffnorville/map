5a6feba5d37be1f6ec752abf10b1eea594d013e2
Jeff Norville <jeff@jnorville.com>
2019-04-16 16:26:25 +0200

    adding rhone-alpes, bourgogne

--- load_postgis.R
+++ load_postgis.R
@@ -1,19 +1,50 @@
 ###load_postgis
+
+## NB passwords in plaintext on git are a liability (even if DBs are local)
+
 rm(list=ls())
 
 library(rpostgis)
 
+# con  <-  dbConnect("PostgreSQL",
+#                    dbname = 'api2',
+#                    host   = 'localhost',
+#                    user   = 'pgisuser',
+#                    password = 'apismal2019')
+
 con  <-  dbConnect("PostgreSQL",
-                   dbname = 'api2',
+                   dbname = 'apismal',
                    host   = 'localhost',
                    user   = 'pgisuser',
                    password = 'apismal2019')
+
+con  <-  dbConnect("PostgreSQL",
+                   dbname = 'apismal',
+                   host   = 'localhost',
+                   user   = 'postgres',
+                   password = 'postgres')
+
+
+con  <-  dbConnect("PostgreSQL",
+                   dbname = 'postgres',
+                   host   = 'localhost',
+                   user   = 'postgres',
+                   password = 'postgres')
+
+
 
 pgPostGIS(con)
 #PostGIS extension version 2.5.1 installed.
 #[1] TRUE
 
 ###load by department
+
+rdaCultFiles <- list.files(path = "C:/opt/donnees_R/RPG/V2", pattern = "ilotsCult*")
+rdaIlotFiles <- list.files(path = "C:/opt/donnees_R/RPG/V2", pattern = "ilots_20*")
+rm(rdaIlotFiles)
+
+rdaCultFiles <- list.files(path = "C:/opt/donnees_R/RPG/V2", pattern = "ilotsCult*")
+rdaIlotFiles <- list.files(path = "C:/opt/donnees_R/RPG/V2", pattern = "ilots_20*")
 
 #load Picardie (002, 060, 080)
 load("C:/opt/donnees_R/RPG/V2/ilotsCult_2008_002.rda")
@@ -73,9 +104,21 @@ load("C:/opt/donnees_R/RPG/V2/ilots_2008_074.rda")
 1. BETTER, convert all incoming to 3035
 
 # CONVERSION COORDS
-str(ilots_2008_021@proj4string)
+ilots3035_2008_002 <- spTransform(ilots_2008_002, "+init=epsg:3035")
+plot(ilots3035_2008_002)
 
-sf_2008_021 <- st_as_sf(
+class(pras)
+sf_pras <- st_as_sf(
+  pras,
+  coord = c('x', 'y'),
+  crs="+init=epsg:3035"
+)
+
+class(ilots_2008_002)
+str(ilots_2008_002@proj4string)
+
+
+sf_2008_008 <- st_as_sf(
   ilots_2008_021,
   coord = c('x', 'y'),
   crs="+init=epsg:32631"
@@ -87,10 +130,30 @@ sf_2008_021_3035 <-  st_transform(sf_2008_021, 3035)
 plot(sf_2008_021_3035)
 class(sf_2008_021_3035)
 
+
+class(ilots_2008_002)
+
 library(rpostgis)
 pgInsert(con, 
          c("public","ilots"), 
-         ilots_2008_089, 
+         ilots_2008_002, 
+         geom = "geom", 
+         df.mode = FALSE,
+         partial.match = FALSE, 
+         overwrite = FALSE, 
+         new.id = NULL,
+         row.names = FALSE, 
+         upsert.using = NULL, 
+         alter.names = FALSE,
+         encoding = NULL, 
+         return.pgi = FALSE, 
+         df.geom = NULL,
+         geog = FALSE)
+
+#  RS-DBI driver: (could not Retrieve the result : ERREUR:  Geometry SRID (3035) does not match column SRID (32631)
+pgInsert(con, 
+         c("public","ilots"), 
+         ilots3035_2008_002, 
          geom = "geom", 
          df.mode = FALSE,
          partial.match = FALSE, 
@@ -105,9 +168,10 @@ pgInsert(con,
          geog = FALSE)
 
 
+
 pgInsert(con, 
-         c("public","pra"), 
-         pras, 
+         c("basemap","pra"), 
+         sf_pras, 
          geom = "geom", 
          df.mode = FALSE,
          partial.match = FALSE, 
@@ -145,16 +209,36 @@ str(ilots_2008_002)
 class(ilotsCult_2008_021$ID_ILOT) #no problem
 class(ilots_2008_021$ID_ILOT) #problem
 
-# 3. BEFORE LOAD - convert ID_ILOT to num from str
-ilots_2008_089$ID_ILOT <- as.numeric(ilots_2008_089$ID_ILOT)
-class(ilots_2008_021$ID_ILOT) #no more problem!!
+#### important step !!! ####
 
-# 4. CONVERT COORDINATES
+# 3. BEFORE LOAD - convert ID_ILOT to num from str
+ilots_2008_002$ID_ILOT <- as.numeric(ilots_2008_002$ID_ILOT)
+ilots_2008_060$ID_ILOT <- as.numeric(ilots_2008_060$ID_ILOT)
+ilots_2008_080$ID_ILOT <- as.numeric(ilots_2008_080$ID_ILOT)
+
+ilots_2008_001$ID_ILOT <- as.numeric(ilots_2008_001$ID_ILOT)
+ilots_2008_007$ID_ILOT <- as.numeric(ilots_2008_007$ID_ILOT)
+ilots_2008_021$ID_ILOT <- as.numeric(ilots_2008_021$ID_ILOT)
+ilots_2008_026$ID_ILOT <- as.numeric(ilots_2008_026$ID_ILOT)
+ilots_2008_038$ID_ILOT <- as.numeric(ilots_2008_038$ID_ILOT)
+ilots_2008_042$ID_ILOT <- as.numeric(ilots_2008_042$ID_ILOT)
+ilots_2008_058$ID_ILOT <- as.numeric(ilots_2008_058$ID_ILOT)
+ilots_2008_069$ID_ILOT <- as.numeric(ilots_2008_069$ID_ILOT)
+ilots_2008_071$ID_ILOT <- as.numeric(ilots_2008_071$ID_ILOT)
+ilots_2008_073$ID_ILOT <- as.numeric(ilots_2008_073$ID_ILOT)
+ilots_2008_074$ID_ILOT <- as.numeric(ilots_2008_074$ID_ILOT)
+ilots_2008_089$ID_ILOT <- as.numeric(ilots_2008_089$ID_ILOT)
+
+
+class(ilots_2008_089$ID_ILOT) #no more problem!!
+
+## 4. CONVERT COORDINATES
+# can do this later within postgis (and faster)
 
 library(rpostgis)
 pgInsert(con, 
          c("public","ilots"), 
-         ilots_2008_080, 
+         ilots_2008_071, 
          geom = "geom", 
          df.mode = FALSE,
          partial.match = FALSE, 
@@ -177,10 +261,12 @@ ilotsCult_2008_002, ilotsCult_2008_060, ilotsCult_2008_080
 summary(ilotsCult_2008_002)
 head(ilotsCult_2008_002)
 
+summary(ilotsCult_2008_002)
+
 
 pgInsert(con, 
-         c("public","ilotsCult"), 
-         ilotsCult_2008_089, 
+         c("public","culture"), 
+         ilotsCult_2008_080, 
          geom = FALSE, 
          df.mode = FALSE,
          partial.match = FALSE, 
