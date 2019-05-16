201b88455a29e5791ac21391beb052fcbcfc2795
jeffnorville <jeff@jnorville.com>
2019-05-16 09:32:50 +0200

    sandboxing sankeys

--- sankey_example.R
+++ sankey_example.R
@@ -8,3 +8,17 @@ tit <- as.data.frame(Titanic)
 alluvial( tit[,1:4], freq=tit$Freq, border=NA,
           hide = tit$Freq < quantile(tit$Freq, .50),
           col=ifelse( tit$Class == "3rd" & tit$Sex == "Male", "blue", "gray") )
+
+
+alluvial( tit[,1:4], freq=tit$Freq, border=NA,
+          hide = tit$Freq < quantile(tit$Freq, .50),
+          col=ifelse( tit$Survived == "Yes" & tit$Age == "Child", "red", "gray") )
+
+alluvial( tit[,1:4], freq=tit$Freq, border=NA,
+          hide = tit$Freq < quantile(tit$Freq, .50),
+          col=ifelse( tit$Survived == "No" & tit$Sex == "Female", "violet", "gray") )
+
+alluvial( tit[,1:4], freq=tit$Freq, border=NA,
+          hide = tit$Freq < quantile(tit$Freq, .50),
+          col=ifelse( tit$Class == "1st" & tit$Sex == "Male", "blue", "grey") )
+
