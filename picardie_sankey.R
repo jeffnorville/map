995b28f10ed819a95832d10459ee09a1bc316d44
jeffnorville <jeff@jnorville.com>
2019-06-14 14:26:53 +0200

    testing sankey diag for picardie by land area

--- picardie_sankey.R
+++ picardie_sankey.R
@@ -2,19 +2,55 @@
 # sankey
 library(networkD3)
 nodes = data.frame("name" = 
-                     c("PRA", # Node 0
-                       "Region1", # Node 1
-                       "Region2", # Node 2
-                       "Dept"))# Node 3
+                     c("Picardie", #node 0
+                       "Dept 2",   #node 1
+                       "Dept 60",  #node 2
+                       "Dept 80",  #node 3
+                       "PRA 02034" ))#node 4
+# ,
+#                        "02034",
+#                        "02328",
+#                        "02323",
+#                        "02336",
+#                        "02317",
+#                        "02329",
+#                        "60327",
+#                        "60329",
+#                        "60043",
+#                        "60041",
+#                        "60042",
+#                        "60331",
+#                        "60328",
+#                        "60330",
+#                        "80327",
+#                        "80035" ))
+                       # "80036",
+                       # "80038",
+                       # "80037")) #node 23
 links = as.data.frame(matrix(c(
-  0, 1, 10, # Each row represents a link. The first number
-  0, 2, 20, # represents the node being conntected from. 
-  1, 3, 30, # the second number represents the node connected to.
-  2, 3, 40),# The third number is the value of the node
-  byrow = TRUE, ncol = 3))
-names(links) = c("source", "target", "value")
+  0, 1, 2, 12.15  ,
+  0, 1, 3,  8.83  ,
+  0, 2, 4,  6.36  ,
+  0, 3, 4,  5.62 ) ,
+  # 0, 1,  8,  2.82  ,
+  # 0, 1,  9,  2.27  ,
+  # 0, 2, 10, 10.25  ,
+  # 0, 2, 11,  5.78  ,
+  # 0, 2, 12,  3.72  ,
+  # 0, 2, 13,  2.48  ,
+  # 0, 2, 14,  2.44  ,
+  # 0, 2, 15,  2.14  ,
+  # 0, 2, 16,  1.88  ,
+  # 0, 2, 17,  1.51  ,
+  # 0, 3, 18, 11.09  ,
+  # 0, 3, 19,  8.74  ,
+  # 0, 3, 20,  5.79  ,
+  # 0, 3, 21,  4.92  ,
+  # 0, 3, 22,  1.21 
+  byrow = TRUE, ncol = 4))
+names(links) = c("region", "dept", "sar", "value")
 sankeyNetwork(Links = links, Nodes = nodes,
-              Source = "source", Target = "target",
+              Source = "region", Target = "dept",
               Value = "value", NodeID = "name",
-              fontSize= 12, nodeWidth = 30)
+              fontSize= 12, nodeWidth = 20)
 
